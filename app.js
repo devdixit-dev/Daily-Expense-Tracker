899b84f247227d976ab926d6231fd21c7057847e
Dev Dixit <devdixitsocial@gmail.com>
2025-04-28 12:35:24 +0530

    error solved in code

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import userRouter from './routes/userRoutes.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 connectDB();
 
